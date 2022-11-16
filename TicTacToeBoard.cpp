1ee7b6fd45d8ac5fa68a5648529138a242d44dd6
manicdepravity <andrewjh1986@outlook.com>
2022-11-16 11:43:12 -0800

    Adding TicTacToe game features along with Unit Tests for work ability

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -4,52 +4,113 @@
  * to represent the spaces on the board.
 **/
 
-//Constructor sets an empty board and specifies it is X's turn first
+
+// Constructor sets an empty board and specifies it is X's turn first
 TicTacToeBoard::TicTacToeBoard()
 {
-  turn = X;
-  for(int i=0; i<BOARDSIZE; i++)
-    for(int j=0; j<BOARDSIZE; j++)
-      board[i][j] = Blank;
+    turn = X;
+    for (int i = 0; i < BOARDSIZE; i++)
+        for (int j = 0; j < BOARDSIZE; j++)
+            board[i][j] = X;
+
+
 }
 
 /**
  * Switches turn member variable to represent whether it's X's or O's turn
  * and returns whose turn it is
-**/
+ **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+    Piece temp = Invalid;
+    Piece x = X;
+    Piece y = O;
+    if (turn == x)
+    {
+        turn = y;
+    }
+    else if (turn == y)
+    {
+        turn = x;
+    }
+    else
+    {
+        turn = temp;
+    }
+    return turn;
 }
+//Resets each board location to the Blank Piece value
+// void TicTacToeBoard::clearBoard()
+// {
 
+// 	for(int i=0; i<3; i++){
+// 		for(int j=0; j<3; j++){
+// 			board[i][j] = Blank;
+// 		}
+// 	}
+// }
 /**
  * Places the piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. placePiece does 
+ * piece is placed, and toggles which Piece's turn it is. placePiece does
  * NOT allow to place a piece in a location where there is already a piece.
- * In that case, placePiece just returns what is already at that location. 
+ * In that case, placePiece just returns what is already at that location.
  * Out of bounds coordinates return the Piece Invalid value. When the game
  * is over, no more pieces can be placed so attempting to place a piece
  * should neither change the board nor change whose turn it is.
-**/ 
+ **/
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+    if (row < 0 || row > 3) return Invalid;
+
+    else if (column < 0 || column > 3){
+        return Invalid;
+
+    }
+    else if (board[row][column] == Blank && turn == X)
+    {
+        board[row][column] = X;
+    }
+    else if (board[row][column] == Blank && turn == O)
+    {
+        board[row][column] = O;
+    }
+//    else
+//        for(int i = 0; i <= 3;i++){
+//            for(int j = 0; j <= 3;j++){
+//                if(board[i][j] > 3 || board[i][j] < 0)
+//                    return Invalid;
+//            }
+//        }
+
+
+    return board[row][column];
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+    if (board[row][column] == Blank)
+    {
+        return Blank;
+    }
+    else if (board[row][column] != Blank && board[row][column] != Invalid)
+    {
+        return board[row][column];
+    }
+    else
+        return Invalid;
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won.
-**/
+ **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+
+return Invalid;
+
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,5 +14,111 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+TEST(TicTacToeBoardTest, smokeTest)
+{
+    ASSERT_TRUE(true);
+}
+// Testing values returned from toggle
+TEST(TicTacToeBoard, toggle)
+{
+
+    Piece temp11 = X;
+    Piece temp12 = O;
+    TicTacToeBoard obj;
+    obj.toggleTurn();
+    ASSERT_EQ( temp11, obj.toggleTurn());
+    ASSERT_EQ(temp12, obj.toggleTurn());
+    ASSERT_TRUE(obj.toggleTurn() == X);
+    ASSERT_TRUE(obj.toggleTurn() == O);
+}
+
+//TEST(TicTacToeBoardTest,board)
+//{
+//    TicTacToeBoard obj;
+//
+//    Piece player1 = X;
+//    Piece player2 = O;
+//    if (player1){
+//    obj.toggleTurn() = player1
+//
+//
+//    if (player2){
+//    obj.toggleTurn() = player2;
+//
+//    }
+//    ASSERT_EQ(player1,player2);
+//
+//    }
+////
+////
+////
+////
+////
+//}
+
+
+TEST(TicTacToeBoard, get)
+{
+    Piece obj = X;
+    Piece obj3 = Blank;
+
+    TicTacToeBoard obj1;
+    int row = 1;
+    int column = 0;
+
+
+    int row1 = -1;
+    int column1 = -1;
+    // testing following includes blank
+    ASSERT_EQ( obj,obj1.getPiece(row, column1));
+    // Testing for invalid moves
+    ASSERT_EQ( obj,obj1.getPiece(row1,column1));
+}
+
+
+TEST(TicTacToeBoard, place)
+{
+
+TicTacToeBoard place1, place2;
+int row = 1;
+int column = 1;
+
+ASSERT_TRUE(place1.placePiece(row,column));
+int row1 = -1;
+
+int column1 = 4;
+int row3 = 0;
+int column3 = 1;
+assert(place1.placePiece(row1,column1));
+// Testing that the following gives an invalid
+ASSERT_EQ(Invalid, place1.placePiece(row1,column1));
+// Testing X is valid in placement
+ASSERT_EQ(X, place2.placePiece(row3,column3));
+}
+
+//TEST(TicTacToeBoardTest, toggleThatTurn)
+//{
+//TicTacToeBoardTest obj;
+////int sleeping[7] = {8,8,8,8,7,7,8};
+//char turn = [0,1]
+//char* actual = obj.toggleTurn(turn = 0);
+//ASSERT_EQ(actual,turn);
+//
+//TEST(TicTacToeBoardTest, toggleThatTurn)
+//{
+//    TicTacToeBoardTest obj;
+//    int player = = obj.TicTacToeBoard::toggleTurn( = 'X');
+//    int* actual = obj.TicTacToeBoard::toggleTurn(turn = 'O');
+//    ASSERT_EQ(actual,turn);
+//}
+
+
+//}
+//TEST(TicTacToeBoardTest, toggleThat)
+//{
+////Practice my;
+////my.toggleTurn
+//}
+
 
 
